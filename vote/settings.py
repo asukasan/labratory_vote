8feb979ebc53e014a4549ceebbe859661cdd9c83
ixtab code <ixtab@ixtabnoMacBook-Pro.local>
2020-09-01 15:56:15 +0900

    a

--- vote/settings.py
+++ vote/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'vote_prototype',
         'USER': 'username',
-        'PASSWORD': 'password',
+        'PASSWORD': 'localhost',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
