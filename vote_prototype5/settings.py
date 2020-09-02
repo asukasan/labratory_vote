d2aad67d81d9f54a365aedc8511442982fbc6aae
ixtab code <ixtab@ixtabnoMacBook-Pro.local>
2020-09-02 16:30:49 +0900

    a

--- vote_prototype5/settings.py
+++ vote_prototype5/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = '(6gutb_i3jie7l2x$+hb@lui_=%+5t)-w)62vup(l_h4z3tuag'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
